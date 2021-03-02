a175e246d091459553caaab97753d479d72f2e17
Erik <kimseyerik@gmail.com>
2021-03-02 18:22:28 -0500

    Getting somewhere with typescript and styled components

--- src/LandingPage/LandingPage.tsx
+++ src/LandingPage/LandingPage.tsx
@@ -1,8 +1,14 @@
+import { log } from 'node:console';
 import React, { FC, useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 type Props = {
     header?: string;
+};
+
+type StyledProps = {
+    bg?: string;
+    textColor?: string;
 };
 
 const LandingPage: FC<Props> = ({ header }) => {
@@ -14,14 +20,15 @@ const LandingPage: FC<Props> = ({ header }) => {
         }
     });
     return (
-        <StyledContainer>
+        <StyledContainer bg="">
             <h1>{header}</h1>
         </StyledContainer>
     );
 };
 
-const StyledContainer = styled.div`
+const StyledContainer = styled.div<StyledProps>`
     border: solid 1px #f0f;
+    background-color: ${(props) => (props.bg ? props.bg : '#a0f')};
     h1 {
         color: blueviolet;
     }
